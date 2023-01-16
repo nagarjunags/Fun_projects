33476b12696a9bf91eac304dc48846c78752d118
Nagarjun <81310485+nagarjunags@users.noreply.github.com>
2023-01-16 22:03:44 +0530

    Add files via upload
    ...

---
+++ auto_login_bot/Readme.md
@@ -0,0 +1,3 @@
+This is a small python script to automatically login to the alvas wifi
+
+-you can configure this code with your own login credentials
---
+++ auto_login_bot/a.py
@@ -0,0 +1,43 @@
+#! /usr/bin/python3
+#--------------------------------------------------------------------------------------------------------------
+  
+  
+  
+  #DEependencies
+  #---> pip3 install os
+  #---> install any web driver(in this case firefox's geckodriver)
+
+
+
+  #------------------------------------------------------------------------------------------------------------
+
+
+import os
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+browser = webdriver.Firefox()  #calling the firefox driver  (installed from gitub)
+
+
+
+browser.get('http://192.168.1.1:8090/') #opens the browser and also open the given link
+
+username = browser.find_element(By.NAME, 'username')  #giving the css_selector path of the username text box(or where user needs to provide input)
+password = browser.find_element(By.NAME, 'password')  # css_selector path of the password
+username.send_keys("Test")  #this will type the username and password
+password.send_keys("Hero@8055")
+#login_button = browser.find_element_by_xpath('//*[@id="loginbutton"]')
+login_button = browser.find_element(By.XPATH, '//*[@id="loginbutton"]')
+#giving the xpath if the login button
+login_button.click()									#clicks the login button
+browser.close()     #closes the browser
+speak="\"connected to Alvas Internet\""
+cmd = "{0} {1}".format("espeak ", speak)# (sudo apt install qrencode)
+os.system(cmd)
+
+#browser=webdriver.Firefox()							#can open the browser for another session							
+#browser.get('https://www.youtube.com/')
+
+#Author-Nagarjuna GS
